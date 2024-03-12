227a15032f8cd45e0c5cd006c0a5fd9c9b7bab1b
deepak-p-j <139530897+deepak-p-j@users.noreply.github.com>
2024-03-12 23:31:33 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built new job\n";
+  court << "Webhook successfully enabled\n";
   return 1;
 }
